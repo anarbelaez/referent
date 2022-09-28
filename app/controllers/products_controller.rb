bbcf0909d3a647e90af621f08c6ea7b5957980c4
camilo ardila <miloardila98@gmail.com>
2022-09-27 19:59:08 -0500

    add create, edit, update and destroy actions in the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,28 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = product.create(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product.destroy
+
+    redirect_to products_path, status: :see_other
+  end
+
+
 end
