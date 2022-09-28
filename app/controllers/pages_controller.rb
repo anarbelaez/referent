57823b271f1ea9dfd453fb4b7999d2a971ad813d
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-28 15:13:30 -0500

    added search functionality (for now it's a test)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,20 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @referents = User.where(role: true)
+    @products = Product.all
+
+    # En proceso
+
+    # "root"=>{"filter"=>"products", "query"=>"dell"
+    # if params[:root].present?
+    #   if params[:root][:filter] == "products"
+    #     @objects = Product.where("name LIKE ?", "%#{params[:root][:query]}%")
+    #   elsif params[:root][:filter] == "referents"
+    #     @objects = User.where(brand: params[:root][:query])
+    #   end
+    # else
+    #   @objects = User.where(role: true)
+    # end
   end
 end
