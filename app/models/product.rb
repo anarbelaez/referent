35e5e767f4fa495fdd9d7a29fcaf2c5936b6535b
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-29 13:42:06 -0500

    validations ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :orders
+
+  validates :name, :price, :stock, :size, :category, :genre, :user, presence: true
+  validates :name, length: { minimum: 6 }
+  validates :description, length: { maximum: 200 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :stock, numericality: { only_integer: true, greater_than: 0 }
+  validates :size, inclusion: { in: %w[XS S M L XL] }
+  validates :category, inclusion: { in: %w[TOP BOTTOM DRESSES SHOES ACCESORIES] }
+  validates :genre, inclusion: { in: %w[WOMEN MEN KIDS UNISEX] }
 end
