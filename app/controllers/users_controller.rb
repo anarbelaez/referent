7c11cbc280025bcef0c9b6b7144f219adada039f
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-29 23:12:29 -0500

    fixed details action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,19 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @referents = User.where(role: true).select(:brand, :description)
+    @referents = User.referents
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def details
+    if current_user.id == params[:user_id].to_i
+      @products = current_user.products
+      @orders = current_user.orders
+    else
+      redirect_to home_path
+    end
   end
 end
