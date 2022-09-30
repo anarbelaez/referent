af5fda36577af60962795da769e676be4b0c9993
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-29 23:13:29 -0500

    added validations and some methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+  validates :brand, uniqueness: true # , on: :update
+  validates :description, length: { maximum: 200 }
+
   has_many :products
   has_many :orders
+
+  scope :referents, -> { where(role: true).select(:id, :brand, :description) }
+  scope :fashion_lovers, -> { where.not(role: true) }
+
+  def referent?
+    role == true
+  end
+
+  def fashion_lover?
+    role == false
+  end
 end
