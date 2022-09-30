b15682d3ef67fbac52291e80f6e27e0d48d5e554
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-29 23:17:22 -0500

    fixed user and pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Pages
-  root to: "pages#home"
+  root to: "pages#home", as: :home
   get "/search", to: "pages#search"
 
   # Users
   get "/referents", to: "users#index"
   resources :users, only: [:show] do
     # get "/products", to: "user#products"
-    get :products
-    get :orders
+    get :details
   end
 
   # Products
