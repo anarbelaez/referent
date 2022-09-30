a68c9057ba86b96bdaaba5f7335ac9cb84e7a3a6
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-29 23:11:50 -0500

    fixed search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,20 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @referents = User.where(role: true)
+    @referents = User.referents
     @products = Product.all
 
-    # En proceso
-
-    # "root"=>{"filter"=>"products", "query"=>"dell"
-    # if params[:root].present?
-    #   if params[:root][:filter] == "products"
-    #     @objects = Product.where("name LIKE ?", "%#{params[:root][:query]}%")
-    #   elsif params[:root][:filter] == "referents"
-    #     @objects = User.where(brand: params[:root][:query])
-    #   end
-    # else
-    #   @objects = User.where(role: true)
-    # end
+    search = params[:root]
+    if search.present?
+      if search[:filter] == "products"
+        @products = Product.where("name LIKE ?", "%#{params[:root][:query]}%")
+      else
+        @referents = User.where(brand: params[:root][:query])
+      end
+    end
   end
 end
