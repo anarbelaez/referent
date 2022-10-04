12a2b3c35b054d13bb80d531565d9968337e640e
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-04 14:50:08 -0500

    added a useful View Helper to manage user policies through the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def referent_user?
+    user_signed_in? && current_user.referent?
+  end
+
+  def referent_product?(product)
+    user_signed_in? && current_user == product.user
+  end
 end
