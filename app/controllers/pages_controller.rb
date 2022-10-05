c9de2e6293b7d0ccdf82c97a067298e7598c7149
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-05 00:24:44 -0500

    added a search method to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,13 +5,19 @@ class PagesController < ApplicationController
     @referents = User.referent
     @products = Product.available
 
-    search = params[:root]
-    if search.present?
-      if search[:filter] == "products"
-        @products = Product.where("name LIKE ?", "%#{params[:root][:query]}%")
-      else
-        @referents = User.where(brand: params[:root][:query])
-      end
+    # search = params[:root]
+    # if search.present?
+    #   if search[:filter] == "products"
+    #     @products = Product.where("name LIKE ?", "%#{params[:root][:query]}%")
+    #   else
+    #     @referents = User.where(brand: params[:root][:query])
+    #   end
+    # end
+  end
+
+  def search
+    if params[:query].present?
+      @results = PgSearch.multisearch(params[:query])
     end
   end
 end
