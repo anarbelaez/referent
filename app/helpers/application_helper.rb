d8fcbfdc0906baa0d76d42bd89e37d8eb7881ba5
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-06 07:42:11 -0500

    added aanother helper method: fashion_lover?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def referent_product?(product)
     user_signed_in? && current_user == product.user
   end
+
+  def fashion_lover?
+    user_signed_in? && current_user.fashion_lover?
+  end
 end
