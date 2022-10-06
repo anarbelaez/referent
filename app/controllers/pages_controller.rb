d9ed6bedb8d061f23fe1fc105c7d507701e5a79c
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-06 17:23:16 -0500

    add variable @query for show in the view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
 
   def search
     if params[:query].present?
+      @query = params[:query]
       @products = Product.search(params[:query]).available
     else
       @products = Product.available
