d6dd1965b71d9728c979ca141ad6a9854f77d8ab
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-07 15:30:00 -0500

    Home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,14 @@ module ApplicationHelper
     user_signed_in? && current_user.fashion_lover?
   end
 
+  def image(object)
+    if object.photo.attached?
+      cl_image_path object.photo.key
+    else
+      "https://www.online-tech-tips.com/wp-content/uploads/2022/03/image-41.jpeg"
+    end
+  end
+
   def can_review?(referent)
     # if current_user.fashion_lover? && current_user.orders.first.closed?
     #   product.brand == referent.brand
